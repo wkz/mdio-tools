wkz/mdio-tools

commit 4c4d48cccd5963e7381c6d76e3006e63f341625e

mvls: Add basic usage text on 'mvls -h' or 'mvls help'
Signed-off-by: Joachim Wiberg <troglobit@gmail.com>

src/mvls/mvls.c
@@ -736,6 +736,32 @@ void env_show_pvt(struct env *env)
 	}
 }
 
+int usage(int rc)
+{
+	fputs("Usage: mvls [CMD]\n"
+	      "\n"
+	      "Commands:\n"
+	      "\n"
+	      "  port\n"
+	      "    Displays and overview of switchcore ports and their properties.\n"
+	      "\n"
+	      "  atu\n"
+	      "    Displays the contents of the ATU with VLAN and port vectors.\n"
+	      "\n"
+	      "  vtu\n"
+	      "    Displays the contents of the VTU with FID and port mappings.\n"
+	      "    Port VLAN membership states: (u)ntagged, (t)agged, =CPU, (D)SA\n"
+	      "\n"
+	      "  pvt [INDEX]\n"
+	      "    Displays the contents of the Port VLAN Table, optionally on the\n"
+	      "    given switchcore index, by default index 0 is shown."
+	      "\n"
+	      "By default, mvls displays an overview of the VTU, ATU and ports.\n"
+	      , stdout);
+
+	return rc;
+}
+
 int main(int argc, char **argv)
 {
 	struct env env;
@@ -751,6 +777,9 @@ int main(int argc, char **argv)
 		env_show_ports(&env);
 		return 0;
 	}
+
+	if (!strcmp(argv[1], "-h") || !strcmp(argv[1], "help"))
+		return usage(0);
 
 	if (!strcmp(argv[1], "port"))
 		env_show_ports(&env);

commit b68306b28c203a20dbd33bd270f0517d97e12b40

mvls: Fix some errors in the usage message
Fixes: 4c4d48cccd59 ("mvls: Add basic usage text on 'mvls -h' or 'mvls help'")

src/mvls/mvls.c
@@ -779,11 +779,22 @@ int usage(int rc)
 	      "\n"
 	      "  vtu\n"
 	      "    Displays the contents of the VTU with FID and port mappings.\n"
-	      "    Port VLAN membership states: (u)ntagged, (t)agged, =CPU, (D)SA\n"
+	      "    VLAN membership states:\n"
+	      "    .   Not a member\n"
+	      "    u   Member, egress untagged\n"
+	      "    t   Member, egress tagged\n"
+	      "    =   Member, egress unmodified\n"
 	      "\n"
-	      "  pvt [INDEX]\n"
-	      "    Displays the contents of the Port VLAN Table, optionally on the\n"
-	      "    given switchcore index, by default index 0 is shown.\n"
+	      "  pvt [DEV]\n"
+	      ""
+	      "    Displays the contents of the Port VLAN Table. Without DEV, a\n"
+	      "    condensed view of the isolation state between all ports is shown:\n"
+	      "    .   Full isolation\n"
+	      "    x   Bidirectional communication allowed\n"
+	      "    <   Only communication from column to row port allowed\n"
+	      "    ^   Only communication from row to column port allowed\n"
+	      "\n"
+	      "    If DEV is supplied, dump the full PVT for that device.\n"
 	      "\n"
 	      "By default, mvls displays an overview of the VTU, ATU and ports.\n"
 	      , stdout);